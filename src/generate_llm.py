fd107ed86cef06654220a1b049df98da693f18c1
Yuma Kishi <obake2ai@gmail.com>
2025-02-06 10:27:36 +0900

    updates

---
+++ src/generate_llm.py
@@ -0,0 +1,35 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import tiktoken
+
+tokenizer = tiktoken.get_encoding('gpt2')
+context_length = 512
+
+def generate_sample():
+      # number of tokens processed in a single batch
+
+    load_path = "/content/drive/MyDrive/LEVI/GPT2/checkpoints/20241206_054506/gpt_model_epoch_14500.pth"  # 保存時のファイル名に合わせる
+    checkpoint = torch.load(load_path, map_location='cuda')
+    state_dict = {key.replace("_orig_mod.", ""): value for key, value in checkpoint['model_state_dict'].items()}
+
+    loaded_model = GPT(
+        vocab_size=checkpoint['config']['vocab_size'],
+        d_model=checkpoint['config']['d_model'],
+        n_heads=checkpoint['config']['n_heads'],
+        n_layers=checkpoint['config']['n_layers'],
+        context_length=context_length,
+        tokenizer=tokenizer
+        ).to('cuda')
+
+    loaded_model.load_state_dict(state_dict)
+    loaded_model.eval()
+
+    with torch.no_grad():
+        input = torch.tensor(tokenizer.encode("I'm praying: "), dtype=torch.long, device='cuda').unsqueeze(0)
+        print(loaded_model.generate(input, max_new_tokens=500)[0])
+
+if __name__ == '__main__':
+    generate_sample()
