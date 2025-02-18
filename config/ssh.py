84710af9c52a804b29ff9b1ff4235b3b78eae769
Yuma Kishi <obake2ai@gmail.com>
2025-02-18 22:26:15 +0900

    add ssh

--- config/ssh.py
+++ config/ssh.py
@@ -8,7 +8,7 @@ SSH_CONFIG = {
     # 各転送先の設定（ここに最大20件程度追加可能）
     'destinations': [
         {
-            'host': 'zero2wh04.local',
+            'host': '192.168.10.33', #zero2wh04.local
             'local_dir': 'outputs/12x6',           # 例：画像生成スクリプトの出力フォルダ
             'remote_dir': '/home/pi/Oracle_LEDmatrix/samples'
         },
