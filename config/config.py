989f70df039e284df8e5e6eb160b553ae07fd132
Yuma Kishi <obake2ai@gmail.com>
2025-03-05 11:11:21 +0900

    add ssh

--- config/config.py
+++ config/config.py
@@ -12,7 +12,7 @@ STYLEGAN_CONFIG = {
     "trunc": 1.0,                # truncation psi
     "save_lat": False,           # latent 保存フラグ
     "verbose": False,
-    "noise_seed": 3030,
+    "noise_seed": 3031,
     "sg_gpu": "cuda:0",
     # アニメーション関連
     "frames": "600-300",          # （未使用だが互換性のため）
